7c4b3a50f382213d2e26c3f21d4ecd48b96962d7
litvinovnikita <nlitvinov924@gmail.com>
2023-05-13 22:15:39 -0400

    Navbar is created

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from "./state";
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
-  persistsStore,
-  persistsReducer,
+  persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -20,12 +20,28 @@ import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react';
 
 
+const persistConfig = {key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }, 
+    }),
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+    
   </React.StrictMode>
 );
 
